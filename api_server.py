f56f5b27435ff22cb1efc4305f44074b9e77ec91
Jorge C. Lucero <lucerojc@gmail.com>
2025-10-15 17:42:48 -0300

    Fix: Correct spectrogram computation syntax

--- api_server.py
+++ api_server.py
@@ -212,8 +212,10 @@ async def synthesize_voice(request: SynthesisRequest):
         audio_base64 = base64.b64encode(audio_bytes.getvalue()).decode()
         
         # Downsample time series for efficient transmission
-        ds = request.downsample_factor
-        time_ds = result.time[::ds]
+        # ds = request.downsample_factor
+        # time_ds = result.time[::ds]
+        
+        time_ds = result.time
         
         # Extract vocal fold displacements
         vf_left = -result.vocal_fold_displacement[::ds, 0]
